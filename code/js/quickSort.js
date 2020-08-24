661f7b0d41c50e58fba63f8f99ef27f5371efb6c
5201314999 <1446418561@qq.com>
2020-08-24 08:52:59 +0800

    增加快排和n 皇后问题

--- code/js/quickSort.js
+++ code/js/quickSort.js
@@ -1,17 +1,27 @@
 //快速排序
-//算法训练,大体思路看了，后面再去看具体操作步骤，发现2者描述的信息量差距有点大。转成具体编码，至少卡住10分钟
+//算法训练,自己想摸索了一小时，然后看教程。。呜呜呜
+
+
 const quickSort=(arr)=>{
-    const sort=(arr,left=0;right=arr.length-1)=>{
-        if(left>=right){
-            return;
+    if(arr.length<=1) return arr;
+    let i=0,j=arr.length-1,x=arr[0]
+    while(i<j){
+        while(i<j&&arr[j]>=x){
+            j--
         }
-        let i=0,j=right,base=arr[0];
-        
-        
-    };
-    if(arr.length)
-    if(arr.length<2){
-        return arr;
+        if(i<j){
+            arr[i++]=arr[j]
+        }
+        while(i<j&&arr[i]<x){
+            i++
+        }
+        if(i<j){
+            arr[j--]=arr[i]
+        }
     }
-    return [...quickSort(arr.slice(0,i)),k,...quickSort(arr.slice(j,arr.length))];
+    arr[i]=x;
+    return [...quickSort(arr.slice(0,i)),x,...quickSort(arr.slice(i+1,arr.length))];
 }
+
+
+
